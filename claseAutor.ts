ab2270157626614356c45b19b415cfcbeb8f03c6
= <dariocabrera@tdf.edu.ar>
2022-09-25 15:42:18 -0300

    añadido clase autor

---
+++ claseAutor.ts
@@ -0,0 +1,17 @@
+class Autor {
+    protected nombre: string;
+    protected apellido: string;
+    protected anioNacimiento: number;
+    //protected librosEscritos: Libros[];
+
+    constructor (auxNombre : string, auxApellido : string, auxAnio? : number ) {
+        this.nombre = auxNombre;
+        this.apellido = auxApellido;
+        if (auxAnio === undefined){
+            this.anioNacimiento = 0;
+        } else {
+            this.anioNacimiento = auxAnio;
+        }
+    }
+
+}
