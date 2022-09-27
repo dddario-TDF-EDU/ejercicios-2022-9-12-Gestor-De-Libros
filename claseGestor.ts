41c2d026cb00a44cae58c336fd170056f011932f
= <dariocabrera@tdf.edu.ar>
2022-09-27 09:53:49 -0300

    casi funcional

--- claseGestor.ts
+++ claseGestor.ts
@@ -15,6 +15,8 @@ class GestorLibros {
         this.baseDeLibros = auxLibros;
     }
 
+    
+
     public busquedaGeneral() {
         console.log("-------------");
         console.log("Bienvenido al menu de busqueda general"); 
@@ -25,26 +27,33 @@ class GestorLibros {
         let opcionCorrecta = false;
         console.log("1 _ realizar busqueda del libro por titulo ");
         console.log("2 _ realizar busqueda del libro por ISBN");
+        console.log("3 _ ingresar nuevo libro");
 
-        
-        while (opcionCorrecta === false) {
-            if (opcionIngresada === 1) {
+        switch (opcionIngresada) {
+            case 1: {
                 let auxTitulo = readlineSync.question("Ingrese el titulo del libro");
                 this.busquedaLibroPorTitulo(auxTitulo);
                 opcionCorrecta = true;
-            } 
-            
-            //puse 2 if porq por alguna razon daba error la declaracion que pedia el ISBN
-            if (opcionIngresada === 2 ) {
+                break;
+            }
+            case 2: {
                 let auxISBN  = readlineSync.question("Ingrese el ISBN del libro");
                 this.busquedaLibroPorISBN(auxISBN);
                 opcionCorrecta = true;
+                break;
+            }
+            case 3: {
+                this.ingresarLibro();
+                break;
+            }
+            default: {
+                this.busquedaGeneral();
+                break;
             }
         }
-        
     }
 
-    public busquedaLibroPorTitulo(nombreLibro: string) {
+    private busquedaLibroPorTitulo(nombreLibro: string) {
         for (let index = 0; index < this.baseDeLibros.length; index++) {
             if (this.baseDeLibros[index].getTitulo() === nombreLibro){
                 this.menuOpciones(index);
@@ -54,7 +63,7 @@ class GestorLibros {
         }
     }
 
-    public busquedaLibroPorISBN(auxISBN : number) {
+    private busquedaLibroPorISBN(auxISBN : number) {
         for (let index = 0; index < this.baseDeLibros.length; index++) {
             if (this.baseDeLibros[index].getISBN() === auxISBN){
                 this.menuOpciones(index);
@@ -62,6 +71,39 @@ class GestorLibros {
             }
             
         }
+    }
+
+    private validarISBN(paramISBN : number): boolean {
+        let ISBNrepetido = false;
+        for (let i = 0; i < this.baseDeLibros.length; i++) {
+            if (paramISBN === this.baseDeLibros[i].getISBN()) {
+                ISBNrepetido = true;
+                return ISBNrepetido;
+            }
+        }
+        return ISBNrepetido;
+    }
+
+    private ingresarLibro() {
+        let readlineSync = require('readline-sync');
+        let auxISBN : number = readlineSync.question("Ingrese el ISBN del libro");
+        if (this.validarISBN(auxISBN) === true) {
+            console.log("ISBN repetido, verifique el numero ingresado");
+        } else {
+            let auxTitulo: string = readlineSync.question("Ingrese el titulo del libro");
+            let auxAutor: string = readlineSync.question("Ingrese el autor del libro");
+            let auxAnio: number = readlineSync.question("Ingrese el año de publicación del libro");
+            let auxCategoria: string = readlineSync.question("Ingrese la categoria del libro");
+            let auxEditorial: string = readlineSync.question("Ingrese la editorial del libro");
+            let nuevoLibro : Libro = new Libro(auxISBN, auxTitulo, auxAutor, auxAnio, auxCategoria, auxEditorial);
+            //ya creado el libro lo agrego al array
+            this.baseDeLibros.push(nuevoLibro);
+            console.log("libro ingresado")
+        }
+    }
+
+    private eliminarLibro(indiceArray : number) {
+        
     }
 
     private menuOpciones(indiceArray : number) {
@@ -79,7 +121,9 @@ class GestorLibros {
         console.log("8 _ cambiar la categoria del libro ");
         console.log("9 _ obtener el año de publicacion");
         console.log("10_ cambiar el año de publicion del libro");
+        console.log("11_ eliminar libro de la lista");
         console.log("-------------");
+        //
         let readlineSync = require('readline-sync');
         let opcionIngresada = readlineSync.question("Ingrese el numero indicado para realizar la opcion deseada.");
         this.opcionesSwitch(indiceArray, opcionIngresada);
@@ -146,6 +190,10 @@ class GestorLibros {
                 let nuevoAnio = readlineSync.question("Ingrese el año de publicacion que reemplazara el actual");
                 this.baseDeLibros[indiceArray].setAnioPublicacion(nuevoAnio);
                 break;
+            }
+
+            case 11: {
+                this.baseDeLibros.splice(indiceArray,1);
             }
 
             default : {
